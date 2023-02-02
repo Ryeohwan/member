33e99e8f00a4163cd2dd86243e396ee5eab05588
안려환 <arh951019@gmail.com>
2023-02-03 08:59:14 +0900

    ref:드디어 깃헙으로 이사완료

---
+++ src/main/java/mpti/domain/member/application/UserService.java
@@ -0,0 +1,112 @@
+package mpti.domain.member.application;
+
+
+import lombok.RequiredArgsConstructor;
+import mpti.domain.member.api.request.UserRequest;
+import mpti.domain.member.api.response.UserResponse;
+import mpti.domain.member.dao.UserRepository;
+import mpti.domain.member.entity.Memo;
+import mpti.domain.member.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+    private EntityManager em;
+    private static final String SUCCESS = "success";
+    private static final String FAIL = "fail";
+    @Transactional(readOnly = true)
+    public boolean isEmailDuplicate(String email) {
+        if (userRepository.existsByEmail(email)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Transactional
+    public String join(UserRequest user) {
+        User result = user.toEntity().builder()
+                .email(user.getEmail())
+                .age(user.getAge())
+                .phone(user.getPhone())
+                .address(user.getAddress())
+                .gender(user.getGender())
+                .name(user.getName())
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .password(user.getPassword())
+                .build();
+        userRepository.save(result);
+        return result.getName();
+    }
+
+    @Transactional(readOnly = true)
+    public UserResponse findByEmail(String email){
+        User user1 = userRepository.findUserByEmail(email);
+        UserResponse result = new UserResponse();
+        result.setEmail(user1.getEmail());
+        result.setAge(user1.getAge());
+        result.setName(user1.getName());
+        result.setPhone(user1.getPhone());
+        result.setAddress(user1.getAddress());
+        result.setCreateAt(user1.getCreateAt());
+        result.setUpdateAt(user1.getUpdateAt());
+        result.setGender(user1.getGender());
+        return result;
+    }
+
+    @Transactional(readOnly = true)
+    public Boolean relog(String email, String name) {
+        if(userRepository.findUserByEmailAndPassword(email,name).getEmail() == null){
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    public String delete(String email, String name){
+        if(userRepository.deleteUserByEmailAndPassword(email,name) == 1){
+            return SUCCESS;
+        }else{
+            return FAIL;
+        }
+    }
+
+    public String update(UserRequest check){
+        String email = check.getEmail();
+        String password = check.getPassword();
+        User temp = userRepository.findUserByEmailAndPassword(email,password);
+        temp.setPhone(check.getPhone());
+        temp.setAddress(check.getAddress());
+        temp.setAge(check.getAge());
+        temp.setUpdateAt(LocalDateTime.now());
+        temp.setPassword(check.getPassword());
+        temp.setGender(check.getGender());
+        temp.setName(check.getName());
+        String result = temp.getName();
+        return result;
+    }
+
+
+//    public List<UserResponse> findList(List<String> formList) {
+//    //
+//        List<User> users = userRepository.findUsersById(formList);
+//        for(User a : users){
+//            UserResponse temp = new UserResponse();
+//            temp.setName(a.getName());
+//            temp.setGender(a.getGender());
+//            temp.setEmail(a.getEmail());
+//
+//        }
+//    }
+}
---
+++ src/main/java/mpti/domain/member/entity/User.java
@@ -0,0 +1,252 @@
+package mpti.domain.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Entity
+@Table
+@Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+//    @Id
+//    @GeneratedValue(generator = "uuid2")
+//    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+//    @Column(name = "user_id", columnDefinition = "BINARY(16)")
+//    private UUID id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Memo> memoList = new ArrayList<>();
+
+    public void addMemo(Memo memo){
+        memoList.add(memo);
+        memo.setUser(this);
+    }
+
+
+    @Column(unique = true)
+    @Email
+    private String email;
+
+    private Role role;
+
+    // 하면 이거 json 으로 파싱할 때 비밀번호 정보는 주지 않는다고 한다.
+    @Column(nullable = false)
+    private String password;
+    private int age;
+    private String gender;
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$" ,message = "핸드폰 번호의 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
+    private String phone;
+    private String address;
+    @DateTimeFormat(pattern = "yyyy-mm-dd")
+    private Date birth;
+    @CreatedDate
+    @Column(name = "create_at")
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
+
+    private LocalDateTime stopUntil;
+
+
+    private String provider;
+
+    @Column(columnDefinition = "integer default 0")
+    private int core,chest, shoulder, biceps, triceps,back,legs,aerobic;
+
+    @Column
+    private String title;
+
+    @Column
+    private String s3Url;
+
+    public User(String title, String s3Url) {
+        this.title = title;
+        this.s3Url = s3Url;
+    }
+    public User() {
+    }
+
+
+    public void setEmail(String email) {
+
+        if(email != null) this.email = email;
+    }
+
+    public void setName(String name) {
+        if(name != null) this.name = name;
+    }
+    public void setPassword(String password) {
+        if(password != null) this.password = password;
+    }
+
+    public void setAge(int age) {
+        if(age != 0) this.age = age;
+    }
+
+    public void setGender(String gender) {
+        if(gender != null)this.gender = gender;
+    }
+
+    public void setPhone(String phone) {
+        if(phone != null) this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        if(address != null) this.address = address;
+    }
+
+
+    public void setUpdateAt(LocalDateTime updateAt) {
+        if(updateAt != null) this.updateAt = updateAt;
+    }
+
+    public void setProvider(String provider) {if(provider != null)this.provider = provider;}
+
+
+    public void setMemoList(List<Memo> memoList) {
+        this.memoList = memoList;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+
+    public void setStopUntil(LocalDateTime stopUntil) {
+        this.stopUntil = stopUntil;
+    }
+
+    public void setCore(int core) {
+        if(updateAt != null)this.core = core;
+    }
+
+    public void setChest(int chest) {
+        if(updateAt != null)this.chest = chest;
+    }
+
+    public void setShoulder(int shoulder) {
+        if(updateAt != null)this.shoulder = shoulder;
+    }
+
+    public void setBiceps(int biceps) {
+        if(updateAt != null)this.biceps = biceps;
+    }
+
+    public void setTriceps(int triceps) {
+        if(updateAt != null)this.triceps = triceps;
+    }
+
+    public void setBack(int back) {
+        if(updateAt != null)this.back = back;
+    }
+
+    public void setLegs(int legs) {
+        if(updateAt != null)this.legs = legs;
+    }
+
+    public void setAerobic(int aerobic) {
+        if(updateAt != null)this.aerobic = aerobic;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setS3Url(String s3Url) {
+        this.s3Url = s3Url;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", role=" + role +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", phone='" + phone + '\'' +
+                ", address='" + address + '\'' +
+                ", createAt=" + createAt +
+                ", updateAt=" + updateAt +
+                ", provider='" + provider + '\'' +
+                '}';
+    }
+
+    //    @Override
+//    public String toString() {
+//        return "User{" +
+//                "id=" + id +
+//                ", ptlog=" + ptlog +
+//                ", email='" + email + '\'' +
+//                ", name='" + name + '\'' +
+//                ", password='" + password + '\'' +
+//                ", age=" + age +
+//                ", gender='" + gender + '\'' +
+//                ", phone='" + phone + '\'' +
+//                ", address='" + address + '\'' +
+//                ", createAt=" + createAt +
+//                ", updateAt=" + updateAt +
+//                '}';
+//    }
+
+//    @Builder
+//    public User(UUID id, String name, Ptlog ptlog, String email, Role role, String password, int age, String gender, String phone, String address, LocalDateTime createAt, LocalDateTime updateAt) {
+//        this.id = id;
+//        this.name = name;
+//        this.ptlog = ptlog;
+//        this.email = email;
+//        this.role = role;
+//        this.password = password;
+//        this.age = age;
+//        this.gender = gender;
+//        this.phone = phone;
+//        this.address = address;
+//        this.createAt = createAt;
+//        this.updateAt = updateAt;
+//    }
+
+    @Builder
+    public User(long id, String name, Memo memo, String email, Role role, String password, int age, String gender, String phone, String address, LocalDateTime createAt, LocalDateTime updateAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.age = age;
+        this.gender = gender;
+        this.phone = phone;
+        this.address = address;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+
+}

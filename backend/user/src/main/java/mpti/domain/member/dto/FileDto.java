575643ef27a6976dd8d29f0677b47eeed02eee19
RyeoRyeo <73810834+Ryeohwan@users.noreply.github.com>
2023-02-23 23:58:54 +0900

    Update FileDto.java

--- backend/user/src/main/java/mpti/domain/member/dto/FileDto.java
+++ backend/user/src/main/java/mpti/domain/member/dto/FileDto.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+// I have to remove this annotation
 @Setter
 public class FileDto {
     private String email;
